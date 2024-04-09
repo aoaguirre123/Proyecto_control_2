f9974b9878d45cb1d97cf12bebb7334a11498c6d
unknown <CETECOM@AOAWSB03LC1119.duoc.acad>
2024-04-09 10:28:08 -0400

    Avance en clases

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-
+// Script para copiar un producto y mostrarlo 20 veces
 if (document.getElementById('mini_producto')) {
   var tarjeta = document.getElementById('mini_producto').outerHTML;
   var tarjetas = '';
@@ -9,3 +8,25 @@ if (document.getElementById('mini_producto')) {
   document.getElementById('mini_producto').outerHTML = tarjetas;
 }
 
+
+// Script para copiar NavBar en cada pagina
+if (document.getElementById('menu')) {
+  fetch('menu_superior.html').then(response => {
+      return response.text();
+  }).then(htmlContent => {
+  document.getElementById('menu').innerHTML = htmlContent;
+  window.scrollTo(0, 0);
+  });
+};
+
+
+// Función para cambiar la imagen de perfil
+function changeProfileImage(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function() {
+        const imgElement = document.getElementById('profile-image');
+        imgElement.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+}
